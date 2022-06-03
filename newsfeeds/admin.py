ed47e34826892b39eb86f5a326d5bcafd9741529
Renee Ju <renee.ju01@gmail.com>
2022-06-03 12:45:15 -0700

    Add newsfeed model

---
+++ newsfeeds/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from newsfeeds.models import Newsfeed
+
+
+@admin.register(Newsfeed)
+class NewsfeedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tweet', 'created_at')
+    date_hierarchy = 'created_at'
+
