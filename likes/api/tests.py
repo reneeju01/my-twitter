d2f77a21e966e46f8d541bbd7fd1f9f626e1842b
Renee Ju <renee.ju01@gmail.com>
2022-06-07 13:23:47 -0700

    Likes cancel api

--- likes/api/tests.py
+++ likes/api/tests.py
@@ -1,8 +1,8 @@
+from rest_framework import status
 from testing.testcases import TestCase
 
-
 LIKE_BASE_URL = '/api/likes/'
-
+LIKE_CANCEL_URL = '/api/likes/cancel/'
 
 class LikeApiTests(TestCase):
 
@@ -16,7 +16,7 @@ class LikeApiTests(TestCase):
 
         # anonymous is not allowed
         response = self.anonymous_client.post(LIKE_BASE_URL, data)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # get is not allowed
         response = self.lucky_client.get(LIKE_BASE_URL, data)
@@ -56,7 +56,7 @@ class LikeApiTests(TestCase):
 
         # anonymous is not allowed
         response = self.anonymous_client.post(LIKE_BASE_URL, data)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # get is not allowed
         response = self.lucky_client.get(LIKE_BASE_URL, data)
@@ -89,3 +89,69 @@ class LikeApiTests(TestCase):
         self.assertEqual(comment.like_set.count(), 1)
         self.cosmo_client.post(LIKE_BASE_URL, data)
         self.assertEqual(comment.like_set.count(), 2)
+
+    def test_cancel(self):
+        tweet = self.create_tweet(self.lucky)
+        comment = self.create_comment(self.cosmo, tweet)
+        like_comment_data = {'content_type': 'comment', 'object_id': comment.id}
+        like_tweet_data = {'content_type': 'tweet', 'object_id': tweet.id}
+        self.lucky_client.post(LIKE_BASE_URL, like_comment_data)
+        self.cosmo_client.post(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # login required
+        response = self.anonymous_client.post(
+            LIKE_CANCEL_URL,
+            like_comment_data,
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # get is not allowed
+        response = self.lucky_client.get(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+        # wrong content_type
+        response = self.lucky_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'wrong',
+            'object_id': 1,
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # wrong object_id
+        response = self.lucky_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'comment',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # cosmo has not liked before
+        response = self.cosmo_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 0)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # successfully canceled
+        response = self.lucky_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 1)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        # lucky has not liked before
+        response = self.lucky_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 0)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        # cosmo's like has been canceled
+        response = self.cosmo_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 1)
+        self.assertEqual(tweet.like_set.count(), 0)
+        self.assertEqual(comment.like_set.count(), 0)
